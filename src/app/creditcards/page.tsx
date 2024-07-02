c1a3ab5ea2ea1556508fa244abfcc753b412c137
Akshar <113759354+akshar1322@users.noreply.github.com>
2024-07-02 14:16:21 +0530

    add card

--- src/app/creditcards/page.tsx
+++ src/app/creditcards/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Card from '../components/layout/Card';
+import data from '../data'; 
 
-type Props = {}
+const CardDetailsPage = () => {
+  const banks = data.banks;
+  const cards = data.cards;
 
-const page = (props: Props) => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">Card Details</h1>
+      <ul className="list-none mb-4">
+        {cards.map((card) => {
+          const bank = banks.find((bank) => bank.id === card.bankId);
+          return (
+            <li key={card.id} className="mb-4">
+              <Card card={card} bank={bank} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default page
+export default CardDetailsPage;
---
+++ src/app/data.js
@@ -0,0 +1,34 @@
+export default {
+    banks: [
+      {
+        id: 1,
+        name: 'Bank of America',
+      },
+      {
+        id: 2,
+        name: 'Chase Bank',
+      },
+      //...
+    ],
+    cards: [
+      {
+        id: 1,
+        name: 'Card 1',
+        bankId: 1,
+        highlight: 'This is a highlight of Card 1',
+        details: 'This is a description of Card 1',
+        features: ['Feature 1', 'Feature 2', 'Feature 3'],
+        applyLink: 'https://example.com/apply/1',
+      },
+      {
+        id: 2,
+        name: 'Card 2',
+        bankId: 2,
+        highlight: 'This is a highlight of Card 2',
+        details: 'This is a description of Card 2',
+        features: ['Feature 4', 'Feature 5', 'Feature 6'],
+        applyLink: 'https://example.com/apply/2',
+      },
+      //...
+    ],
+  };
