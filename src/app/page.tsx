89bac4a3b79529e5040c63f86fa6c60c756bfba4
developerjay18 <jayrai3003@gmail.com>
2024-07-24 22:09:17 +0530

    adding products on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Header from './components/layout/Header';
 import ProductCard from './components/layout/Prodctcard';
 import Slider from './components/layout/Slider';
 import { useRouter } from 'next/navigation'
+import axios from "axios";
+import Link from "next/link";
 
 interface Slide {
   id: number;
@@ -64,20 +66,15 @@ const Home: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/products');
-        const data = await response.json();
-
-        if (Array.isArray(data)) {
-          setProductData(data);
-        } else {
-          console.error('API response is not an array:', data);
-        }
+        const response = await axios.get('/api/products')
+        console.log(response.data)
+        console.log(response.data.data)
+        await setProductData(response.data.data)
       } catch (error) {
-        console.error('Error fetching product data:', error);
+        console.log('ERROR WHILE FETCHING PRODUCTS ON FROTNEND', error)
       }
-    };
-
-    fetchData();
+    }
+    fetchData()
   }, []);
 
   const handleExplore = (productId: string) => {
@@ -90,10 +87,54 @@ const Home: React.FC = () => {
       <Header />
       <br /><br />
       <Slider slides={slidesData} />
-      <div className="flex flex-wrap">
-        {ProductData.map((product) => (
-          <ProductCard key={product.ProdctId} product={product} onExplore={() => handleExplore(product.ProdctId)} />
-        ))}
+      <div className="">
+        {ProductData ? (
+          <div className="flex justify-center">
+            {ProductData.map((item: any, index: any) => (
+              <div
+                className="w-full md:w-1/2 lg:w-1/3 p-4"
+                key={`product-${index + 1}`}
+              >
+                <Link href={`/products/${item.productId}`}>
+                  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
+                    <Image
+                      src={item.imageUrl || ''}
+                      alt={item.name}
+                      width={500}
+                      height={300}
+                      className="w-full h-48 object-cover text-black"
+                    />
+                    <div className="p-6">
+                      <div className="mb-4">
+                        <span
+                          className={`bg-${'#000'}-200 text-${'#000'}-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded text-black`}
+                        >
+                          {item.tag}
+                        </span>
+                      </div>
+                      <h3 className="text-lg text-black font-semibold mb-2">
+                        {item.name}
+                      </h3>
+                      <p className="text-gray-600 mb-4">${item.regularPrice}</p>
+                      <p className="text-gray-800 mb-4">{item.highlight}</p>
+                      <button
+                        onClick={() => handleExplore(item.productId)}
+                        className="text-sm font-semibold text-indigo-500 hover:text-indigo-600 flex items-center space-x-1 transition duration-300 ease-in-out group"
+                      >
+                        <span>Explore</span>
+                        <span className="  transform transition-transform duration-300 group-hover:-rotate-45 ease-in-out text-black ">
+                          →
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="loader"></div>
+        )}
       </div>
       <Footer />
     </main>
