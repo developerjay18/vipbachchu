383c58baa7ed64849b52a90e0f0b1c7a92568b9d
jay rai <jayrai3003@gmail.com>
2024-07-27 09:48:03 +0530

    updating database connection logic

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,31 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-/*
-type ConnectionObject = {
-  isConnected?: number;
-};
-
-const connection: ConnectionObject = {};
-
-async function connectDB(): Promise<void> {
-  if (connection.isConnected) {
-    console.log("Already connected to Database.");
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGODB_URI || "");
-    connection.isConnected = db.connections[0].readyState;
-
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.log("Error connecting to Database");
-    process.exit(1);
-  }
-}
-*/
-
-dotenv.config();
 
 type ConnectionObject = {
   isConnected?: number;
