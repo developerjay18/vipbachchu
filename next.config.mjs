2eb005860ffa5f84d6bdf16ac67e88e056d44fbe
Akshar <113759354+akshar1322@users.noreply.github.com>
2024-07-28 02:33:25 +0530

    chore: Update image domain whitelist with additional domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pexels.com', 'res.cloudinary.com', 'images.unsplash.com', 'm.media-amazon.com','amazon.com','www.amazon.in', 'upload.wikimedia.org','wikimedia.org','img.freepik.com','www.google.com','unboxdiaries.b-cdn.net','asset.cloudinary.com' ],
+    domains: ['pexels.com', 'res.cloudinary.com','asset.cloudinary.com', 'images.unsplash.com', 'm.media-amazon.com','amazon.com','www.amazon.in', 'upload.wikimedia.org','wikimedia.org','img.freepik.com','www.google.com','unboxdiaries.b-cdn.net','etimg.etb2bimg.com','s3-nothing-prod.s3.eu-central-1.amazonaws.com','in.nothing.tech' ],
   },
 }
 
